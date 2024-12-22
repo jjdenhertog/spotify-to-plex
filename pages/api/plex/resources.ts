77c07a37b8bbd1898b94d9f668cd0c82cb45977c
Jan J <jjdenhertog@gmail.com>
2024-12-22 12:31:31 +0100

    Bugfix: HTTPS connections with Plex

--- pages/api/plex/resources.ts
+++ pages/api/plex/resources.ts
@@ -26,22 +26,30 @@ const router = createRouter<NextApiRequest, NextApiResponse>()
 
                 const result = await axios.get(`https://plex.tv/api/v2/resources`, {
                     params: {
-                        "X-Plex-Product=": "AI Guest DJ",
+                        "X-Plex-Product": "Spotify to Plex",
                         "X-Plex-Client-Identifier": process.env.PLEX_APP_ID,
                         "X-Plex-Token": plex.settings?.token,
                     }
                 })
 
+
                 const servers: GetPlexResourcesResponse[] = [];
                 result.data.forEach((item: any) => {
                     if (item.product == "Plex Media Server") {
+
+                        const connections = item.connections.map((connection: any) => {
+                            const { local } = connection;
+                            let { uri } = connection
+                            if (item.httpsRequired)
+                                uri = uri.split('http://').join('https://')
+
+                            return { uri, local }
+                        })
+
                         servers.push({
                             name: item.name,
                             id: item.clientIdentifier,
-                            connections: item.connections.map((connection: any) => ({
-                                uri: connection.uri,
-                                local: connection.local
-                            }))
+                            connections
                         })
                     }
                 })
